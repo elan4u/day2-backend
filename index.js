bb4a61ec45829b4d773987b73eada55c4e85c6c3
Elan J <elandirayan.janarthanan@marlabs.com>
2018-06-17 11:14:48 +0530

    latestfile

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.db);
+require('dotenv').config();
 app.set('port',process.env.PORT || 3000);
 
 app.get('/',((req,res)=>{

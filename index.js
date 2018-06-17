8d57bc2fd6b5ae116312f622cd209887d1ac5d76
Elan J <elandirayan.janarthanan@marlabs.com>
2018-06-17 10:48:12 +0530

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+
 app.set('port',process.env.PORT || 3000);
-app.get('/',(req,res)=>{
+
+app.get('/',((req,res)=>{
     console.log("backend is Called.");
-    res.send("backend is called");
-})
+    res.send("backend for App is requested");
+}));
 app.listen(app.get('port'), () => {
 	console.log('Server Started at ' + app.get("port"));
 });

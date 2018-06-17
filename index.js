484f052ae662da681edf706e720085ebd38bc784
Elan J <elandirayan.janarthanan@marlabs.com>
2018-06-17 10:51:12 +0530

    Json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port',process.env.PORT || 3000);
 
 app.get('/',((req,res)=>{
     console.log("backend is Called.");
-    res.send("backend for App is requested");
+    res.json({message:"backend for App is requested"});
 }));
 app.listen(app.get('port'), () => {
 	console.log('Server Started at ' + app.get("port"));
